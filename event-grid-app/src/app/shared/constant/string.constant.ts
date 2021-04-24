c8691bd7299f26a1e4fb3efc0bf1018458c1c167
Ranjoy <code@rollingarray.co.in>
2021-04-24 13:31:24 +0530

    app initial setup

---
+++ event-grid-app/src/app/shared/constant/string.constant.ts
@@ -0,0 +1,30 @@
+export class StringKey {
+    public static readonly  APP_NAME: string = 'Event Grid';
+    public static readonly  APP_VERSION: string = '1.0.28';
+    
+    public static readonly  EVENTS: string = 'Event(s)';
+    public static readonly  RECEIVE_NOTIFICATION: string = 'Allow Notification(s)';
+    public static readonly  CLOSE: string = 'Close';
+    
+    
+    public static readonly  ICON_BASE_PATH : string =  "assets/custom-ion-icons/";
+    public static readonly  ICON_MENU : string =  StringKey.ICON_BASE_PATH + "menu.svg";
+    public static readonly  ICON_SPEAKER : string =  StringKey.ICON_BASE_PATH + "speaker.svg";
+    public static readonly  ICON_NOTIFICATION : string =  StringKey.ICON_BASE_PATH + "notifications.svg";
+    
+
+    public static readonly  IMAGE_BASE_PATH : string =  "assets/images/";
+    public static readonly  IMAGE_LOADER : string =  StringKey.IMAGE_BASE_PATH + "loader.svg";
+    public static readonly  DEFAULT_IMAGE : string =  StringKey.IMAGE_BASE_PATH + "event-placeholder.jpeg";
+    
+    public static readonly  COLOR_PRIMARY : string =  "primary";
+    public static readonly  COLOR_SECONDARY : string =  "secondary";
+    public static readonly  COLOR_TERNARY : string =  "tertiary";
+    public static readonly  COLOR_QUATERNARY : string =  "quaternary";
+    public static readonly  COLOR_SUCCESS : string =  "success";
+    public static readonly  COLOR_WARNING : string =  "warning";
+    public static readonly  COLOR_DANGER : string =  "danger";
+    public static readonly  COLOR_DARK : string =  "dark";
+    public static readonly  COLOR_MEDIUM : string =  "medium";
+    public static readonly  COLOR_LIGHT : string =  "light";
+  }
